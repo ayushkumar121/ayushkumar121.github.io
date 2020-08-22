3670d7e6d82fb4fa4f6c68214845727589daae26
ayushkumar121 <ayushkumar121@outlook.com>
2020-08-22 14:10:56 +0530

    more animations

--- scripts/step6.js
+++ scripts/step6.js
@@ -9,21 +9,57 @@ var readingTitration = 0.0
 var handle = null
 
 var currentFrame = 16
+var buretteFilled = false
+
+var frameObject5 = { i: 0 }
+
+function buretteFill() {
+    if (!buretteFilled) {
+        var anim = gsap.timeline()
+
+        anim
+            .to('#naoh small', { opacity: 0 })
+            .to('#naoh', { duration: 2, y: -220 })
+            .to('#naoh', { duration: 2, x: -170 })
+            .to('#naoh', {
+                duration: 2, rotation: -20, onComplete: () => {
+                    gsap.to(frameObject5, {
+                        duration: 4, i: 16, ease: new SteppedEase.config(16), onUpdate: () => {
+                            document.querySelector('#burettestep6').src = `./assets/burette/ff${frameObject5.i}.png`
+                        }
+                    })
+                }
+            })
+            .to('#naoh', { duration: 2, rotation: 0 })
+            .to('#naoh', { duration: 4, x: 0, y: 0 })
+            .to('#step-6 button.element', { opacity: 1 })
+    }
+}
+
 
 function openKnob() {
-    if (handle == null) {
+    if (handle == null && !buretteFilled) {
         handle = setInterval(() => {
             if (readingTitration < 50.0) {
                 readingTitration += 0.1
-                document.querySelector('#readingSlider').style.width = `${100 - readingTitration*2}%`
-                
+                document.querySelector('#readingSlider').style.width = `${100 - readingTitration * 20}%`
+
+                var drop = document.createElement("img")
+                drop.src = './assets/drop.png'
+                drop.classList.add('element', 'drop-step-6')
+                drop.style.zIndex = 5
+
+                document.querySelector('#step-6 .instruments').appendChild(drop)
+
+                gsap.to(drop, { y: 50, opacity: 0, ease: Sine.easeIn })
+
                 var text = ''
                 if (Math.abs(prefferedReading[selectedSample] - readingTitration) <= 0.2) {
                     text += '(Titration Complete) '
                 }
 
-                if(currentFrame > 0) {
-                    currentFrame-- 
+                if (currentFrame > 0) {
+                    currentFrame--
 
                     document.querySelector('#burettestep6').src = `./assets/burette/ff${currentFrame}.png`
                 }
@@ -43,10 +79,10 @@ function closeKnob() {
 
         if (Math.abs(prefferedReading[selectedSample] - readingTitration) <= 0.2) {
             task_done = true
-            
+
             document.querySelector('#observationReading').innerHTML = `${readingTitration.toPrecision(2)} ml`
 
-            var result = ( readingTitration.toPrecision(2) * 0.0282 * 10 ) / 2
+            var result = (readingTitration.toPrecision(2) * 0.0282 * 10) / 2
 
             document.querySelector('#result').innerHTML = `<b>Result : ${result.toPrecision(4)}</b>`
 
