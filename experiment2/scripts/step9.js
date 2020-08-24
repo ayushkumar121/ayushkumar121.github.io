a951948d337bb21b9a0d7f33d64ef3a75a450f06
ayushkumar121 <ayushkumar121@outlook.com>
2020-08-24 11:59:49 +0530

    more changes

--- experiment2/scripts/step9.js
+++ experiment2/scripts/step9.js
@@ -61,7 +61,7 @@ function closeKnob1() {
             document.querySelector('#result').innerHTML = `<b>Result : ${peroxide.toPrecision(4)}</b>`
 
             addTask('<b>Step 8</b> Titrate the mixture with 0.1 N sodium thio-sulphate solution until yellow color is almost gone.')
-            addTask('🎉 Experiment Complete 🎉')
+            addTask('🎉 Experiment Completed 🎉')
         }
     }
 }
