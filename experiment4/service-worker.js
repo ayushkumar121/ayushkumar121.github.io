67fe893a6b34208e787f7126218267eaf78fdfee
ayushkumar121 <ayushkumar121@outlook.com>
2020-08-29 19:08:32 +0530

    sw fix 2

--- experiment4/service-worker.js
+++ experiment4/service-worker.js
@@ -1,18 +1,18 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
     '/',
-    '/assets/step1/juice/0.svg',
-    '/assets/step1/juice/1.svg',
-    '/assets/step1/juice/2.svg',
-    '/assets/step1/juice/3.svg',
-    '/assets/step1/juice/4.svg',
-    '/assets/step1/juice/5.svg',
-    '/assets/step1/juice/6.svg',
-    '/assets/step1/juice/7.svg',
-    '/assets/step1/juice/8.svg',
-    '/assets/step1/juice/9.svg',
-    '/assets/step1/juice/10.svg',
-    '/assets/step1/juice/11.svg',
+    'assets/step1/juice/0.svg',
+    'assets/step1/juice/1.svg',
+    'assets/step1/juice/2.svg',
+    'assets/step1/juice/3.svg',
+    'assets/step1/juice/4.svg',
+    'assets/step1/juice/5.svg',
+    'assets/step1/juice/6.svg',
+    'assets/step1/juice/7.svg',
+    'assets/step1/juice/8.svg',
+    'assets/step1/juice/9.svg',
+    'assets/step1/juice/10.svg',
+    'assets/step1/juice/11.svg',
 ]
 
 self.addEventListener('install', (event) => {
