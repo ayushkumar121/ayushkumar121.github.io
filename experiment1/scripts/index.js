0b540ec1fd6286d0cf687bdb0902da184eca9583
ayushkumar121 <ayushkumar121@outlook.com>
2020-08-29 18:30:33 +0530

    sw added

--- experiment1/scripts/index.js
+++ experiment1/scripts/index.js
@@ -33,13 +33,3 @@ setInterval(() => {
         controls.classList.remove('active')
 
 }, 100)
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
