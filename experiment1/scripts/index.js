7a4bf9373d05cd458bed8c87a66555f884eb87e4
ayushkumar121 <ayushkumar121@outlook.com>
2020-08-30 19:58:12 +0530

    exp 1 fix

--- experiment1/scripts/index.js
+++ experiment1/scripts/index.js
@@ -2,7 +2,7 @@ var curent_step = 0
 var steps = document.querySelectorAll('.step')
 var taskslist = document.querySelector('#taskslist')
 var controls = document.querySelector('#controls')
-var task_done = false
+var task_done = true
 
 function next() {
     if (curent_step < steps.length - 1) {
