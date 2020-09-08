452a1f9a0900aecab503ac8a42380b6eb5f56aa7
ayushkumar121 <ayushkumar121@outlook.com>
2020-09-08 18:51:16 +0530

    exp 6

--- experiment6/scripts/step2.js
+++ experiment6/scripts/step2.js
@@ -1,5 +1,5 @@
 let lottie3 = bodymovin.loadAnimation({
-    container: document.querySelector('#step-3 .bodymovinanim'),
+    container: document.querySelector('#step-2 .bodymovinanim'),
     renderer: 'svg',
     loop: false,
     autoplay: false,
@@ -13,18 +13,18 @@ function platySegments2() {
         if (currentSegment2 == 1) {
             lottie3.playSegments([0, 190], true)
             
-            gsap.to('#step-3 .control-1', {opacity: 0})
-            gsap.to('#step-3 .control-2', {delay: 6, opacity: 1, onComplete: () => {
+            gsap.to('#step-2 .control-1', {opacity: 0})
+            gsap.to('#step-2 .control-2', {delay: 6, opacity: 1, onComplete: () => {
                 currentSegment2++
             }})
         }
         else if (currentSegment2 == 2) {
             lottie3.playSegments([190, 508], true)
             
-            gsap.to('#step-3 .control-2', {opacity: 0})
-            gsap.to('#step-3 .control-2', {delay: 6, opacity: 0, onComplete: () => {
+            gsap.to('#step-2 .control-2', {opacity: 0})
+            gsap.to('#step-2 .control-2', {delay: 6, opacity: 0, onComplete: () => {
                 task_done = true
-                addTask('Step2: Add 5 ml of HPO<sup>3</sup> solution in the conical flask')
+                addTask('Step2: Take 5 ml of standard ascorbic acid solution in the conical flask')
             }})
         }
     }
