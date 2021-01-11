d957b6f14686ac130cb23f11edd211f5e1fdcdde
ayushkumar121 <ayushkumar121@outlook.com>
2021-01-11 23:43:53 +0530

    removed virtual labs

--- experiment3/scripts/step3.js
+++
@@ -1,8 +0,0 @@
-function tare() {
-    if (!task_done) {
-        document.querySelector('#step-3 .reading-indicator').innerHTML = '0.0 gm'
-
-        task_done = true
-        addTask('<b>Step 3</b> Press ‘Tare’ button of the analytical balance to make it zero')
-    }
-}
